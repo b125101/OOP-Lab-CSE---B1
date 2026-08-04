bf88337f7401bd660b6a94bd2537831e09c3d23b
b125101 <b125101@iiit-bh.ac.in>
2026-08-04 20:45:08 +0530

    Add files via upload

---
+++ q9 pointer to structure.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct Student {
+    int rollNo;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr = &s;   // pointer to structure
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &ptr->rollNo);
+
+    printf("Enter Name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &ptr->marks);
+
+    printf("\nStudent Details:\n");
+    printf("Roll Number: %d\n", ptr->rollNo);
+    printf("Name       : %s\n", ptr->name);
+    printf("Marks      : %.2f\n", ptr->marks);
+
+    return 0;
+}
